d71b54697248d7cc5a74763cc3fcc8feb0c70631
John Eskew <jeskew@edx.org>
2017-07-26 15:05:33 -0400

    Review feedback.

--- edx_oauth2_provider/__init__.py
+++ edx_oauth2_provider/__init__.py
@@ -2,6 +2,6 @@
 OAuth2 provider based on `django-oauth2-provider` with customizations
 for the edx-platform.
 """
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import unicode_literals
 
 __version__ = '1.2.1'
